2728d0e3120ef31aacff2770d995bde2c0d89d7e
Your Name <you@example.com>
2017-12-17 10:09:05 +0000

    test

--- hw3/free_mem.c
+++ hw3/free_mem.c
@@ -1,3 +1,4 @@
+#include <sys/time.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,16 +8,28 @@
 
 int main(int argc, char **argv) {
     srand(time(NULL));
+    struct timeval sv, endv;
+    struct timezone sz, endz;
     pid_t pid = getpid();
     int i=0, j=0;
     int num = atoi(argv[1]);
     char **mem = NULL;
-    fprintf(stderr, "PID: %d\n", (int)pid);
+    fprintf(stderr, "PID: %d\nPress ENTER to start.", (int)pid);
+    getchar();
+    gettimeofday(&sv, &sz);
     mem = (char**)malloc(sizeof(char**)*num);
     for (i=0; i<num; ++i) mem[i] = (char*)malloc(GB);
-    for (i=0; i<num; ++i) for (j=0; j<GB; ++j) mem[i][j] = rand();
+    for (i=0; i<num; ++i) {
+        for (j=0; j<GB; ++j) mem[i][j] = rand();
+        fprintf(stderr, "Allocated %d GB of memory!\n", i+1);
+    }
+    gettimeofday(&endv, &endz);
+    float elapsed = (endv.tv_sec-sv.tv_sec)+(endv.tv_usec-sv.tv_usec)*1e-6;
+    fprintf(stderr, "Allocated %d GB of memory in %.2f seconds.\nPress ENTER to release memory.\n", num, elapsed);
+    getchar();
     for (i=0; i<num; ++i) free(mem[i]);
     free(mem);
+    fprintf(stderr, "Released %d GB of memory.\nProgram exit normally.", num);
     mem = NULL;
     return 0;
 }
